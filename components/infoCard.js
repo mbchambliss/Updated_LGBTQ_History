e06aa7edc8f27ccbb914506da3731f8982c2fb66
mbchambliss <mbchambliss@gmail.com>
2021-10-18 18:37:30 -0500

    First commit attempt

--- components/infoCard.js
+++ components/infoCard.js
@@ -1,13 +1,82 @@
 import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import { View, StyleSheet, Text, Image, SafeAreaView } from 'react-native';
+import {Button} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 function InfoCard() {
 	return(
 	<SafeAreaView>
-		<Text>TESTING NEW APP</Text>
+		<View style={styles.card}>
+		  	<Text style={styles.title}>
+		        Bullshit
+		  	</Text>
+
+		    <Text style={styles.content}>
+		    	DATE HERE
+		    </Text>
+		  	        
+		    <Text style={styles.content}>
+			    SHORT DESCRIPTION HERE
+			</Text>
+
+			<View style={styles.align}>
+				<Text style={styles.more}>
+			    	More Info
+			    </Text>
+
+		      <Icon.Button
+		        name="heart"
+		        textAlign="center" 
+		        color="white" 
+		        backgroundColor="#FF69B4"
+		    	/>
+
+		    </View>
+		</View>	
 	</SafeAreaView>
 	);
 }
 
 export default InfoCard;
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#FF69B4',
+    width: '85%',
+    alignSelf: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  title: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    paddingTop: 4,
+    fontSize: 18,
+  },
+  content: {
+    color: 'white',
+    textAlign: 'center',
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+  },
+  heart:{
+  	width: 30, 
+  	height: 30,
+  },
+  more:{
+  	color: 'white',
+  	paddingLeft: 8,
+  	fontSize: 16,
+  	fontWeight: 'bold',
+  	paddingRight: '62%',
+  },
+  align: {
+  	flexDirection: 'row',
+  },
+});
