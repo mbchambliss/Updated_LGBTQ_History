657cc63ecaa08099dcb67359500ab8a9470cfd83
mbchambliss <mbchambliss@gmail.com>
2021-10-17 20:10:39 -0500

    First commit for updated LGBTQ app

---
+++ components/infoCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+
+function InfoCard() {
+	return(
+	<SafeAreaView>
+		<Text>TESTING NEW APP</Text>
+	</SafeAreaView>
+	);
+}
+
+export default InfoCard;
