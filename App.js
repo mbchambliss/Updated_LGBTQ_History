17197eaeb010c4fb045bd2faf094993d0cf367ea
Blake Chambliss <blake@roompact.com>
2021-10-30 16:57:23 -0500

    removed screens from folder into 1 file

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { navigation } from '@react-navigation/native';
 
-// import { HomeScreen } from './screens/home.js';
-// import { AboutScreen } from './screens/about.js';
-import { HomeScreen, AboutScreen } from './screens';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import { HomeScreen, AboutScreen, AccountScreen } from './screens';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +13,30 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="LGBTQ History" component={HomeScreen} />
-        <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen name="HomeScreen"
+          component={HomeScreen}
+          options={({ navigation }) => ({
+            title: "LGBTQ+ History",
+            headerLeft: () => (
+              <Icon.Button
+                name="align-justify"
+                color="#FF69B4"
+                backgroundColor="#fff"
+                onPress={() => navigation.push('AccountScreen')}
+              />
+            ),
+            headerRight: () => (
+              <Icon.Button
+                name="user"
+                color="#FF69B4"
+                backgroundColor="#fff"
+                onPress={() => navigation.push('AccountScreen')}
+              />
+            ),
+          })}
+        />
+        <Stack.Screen name="AboutScreen" component={AboutScreen} options={{ title: "About" }} />
+        <Stack.Screen name="AccountScreen" component={AccountScreen} options={{ title: "Account" }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
