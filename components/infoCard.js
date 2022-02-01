eb42ef7d1322c58773bac9c28fea065e9ef538a1
Blake Chambliss <blake@roompact.com>
2022-02-01 09:45:17 -0600

    updates to automated data feed

--- components/infoCard.js
+++ components/infoCard.js
@@ -1,81 +1,57 @@
-import React from 'react';
-import { View, StyleSheet, Text, Image, SafeAreaView } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, Linking, SafeAreaView, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import styles from '../stylesheets/infocard_styles';
+
+import { db } from '../firebase';
+import { collection, onSnapshot, doc, getDocs, query, where, getDoc } from 'firebase/firestore';
 
 
 function InfoCard() {
+
+	const [events, setEvents] = useState([]);
+
+	const getCurrentDate = () => {
+		var date = new Date().getDate();
+		var month = new Date().getMonth() + 1;
+		return month + '-' + date;
+	}
+
+	let date = getCurrentDate();
+	const eventsCollectionRef = collection(db, "events", date, "this_day");
+
+	useEffect(() => {
+		const getEvents = async () => {
+			const data = await getDocs(eventsCollectionRef);
+			console.log(JSON.stringify(data));
+			setEvents(data.docs.map((doc) => ({ ...doc.data(), id: doc.data.id })));
+		}
+		getEvents();
+	}, []);
+
 	return (
-		<SafeAreaView>
-			<View style={styles.card}>
-				<Text style={styles.title}>
-					Bullshit
-		  	</Text>
-
-				<Text style={styles.content}>
-					DATE HERE
-		    </Text>
-
-				<Text style={styles.content}>
-					SHORT DESCRIPTION HERE
-			</Text>
-
-				<View style={styles.align}>
-					<Text style={styles.more}>
-						More Info
-			    </Text>
-
-					<Icon.Button
-						name="heart"
-						textAlign="center"
-						color="white"
-						backgroundColor="#FF69B4"
-					/>
-
-				</View>
-			</View>
-		</SafeAreaView>
+		<View >
+			{events.map((event) => {
+				return (
+					<View style={styles.card} key={event.id}>
+						<Text style={styles.title}>{event.title}</Text>
+						<Text style={styles.content}>{event.date}</Text>
+						<Text style={styles.content}>{event.short_desc}</Text>
+						<View style={styles.align}>
+							<Text style={styles.more}
+								onPress={() => Linking.openURL(event.link)}>More Info</Text>
+							<Icon.Button
+								name="heart-outline"
+								textAlign="center"
+								color="white"
+								backgroundColor="#FF69B4"
+							/>
+						</View>
+					</View>
+				)
+			})}
+		</View >
 	);
 }
-
 export default InfoCard;
-
-const styles = StyleSheet.create({
-	card: {
-		backgroundColor: '#FF69B4',
-		width: '85%',
-		alignSelf: 'center',
-		marginTop: 20,
-		marginBottom: 20,
-		borderTopLeftRadius: 10,
-		borderTopRightRadius: 10,
-		borderBottomLeftRadius: 10,
-		borderBottomRightRadius: 10,
-	},
-	title: {
-		textAlign: 'center',
-		color: 'white',
-		fontWeight: 'bold',
-		paddingTop: 4,
-		fontSize: 18,
-	},
-	content: {
-		color: 'white',
-		textAlign: 'center',
-		paddingVertical: 8,
-		paddingHorizontal: 8,
-	},
-	heart: {
-		width: 30,
-		height: 30,
-	},
-	more: {
-		color: 'white',
-		paddingLeft: 8,
-		fontSize: 16,
-		fontWeight: 'bold',
-		paddingRight: '62%',
-	},
-	align: {
-		flexDirection: 'row',
-	},
-});
