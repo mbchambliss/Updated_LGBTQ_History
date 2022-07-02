66ddd9700160884869b6c1b9d0a9dd836085a4ca
Blake Chambliss <blake@roompact.com>
2022-07-02 14:05:29 -0500

    remove caldropdown and update infocard img

--- components/infoCard.js
+++ components/infoCard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, Linking } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+// import Icon from 'react-native-vector-icons/Ionicons';
 import styles from '../stylesheets/infocard_styles';
 import { db } from '../firebase';
 import { collection, getDocs } from 'firebase/firestore';
